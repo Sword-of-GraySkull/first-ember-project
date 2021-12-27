19203e732bdfbb92d854bd696535c7ae9ed3fc4b
Sword-of-GraySkull <k7raj888@gmail.com>
2021-12-27 11:21:17 +0530

    programmer_name_change

--- app/routes/programmers.js
+++ app/routes/programmers.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class ProgrammersRoute extends Route {
   model() {
-    return ['Kesavaraj', 'Harikishore', 'Lakshay', 'Lokesh'];
+    return ['Kesavaraj', 'Harikishore', 'Lakshay Madhav', 'Lokesh siva'];
   }
 }
